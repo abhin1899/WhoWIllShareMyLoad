d7e5060f0f369622ee00fa1343c0a84da94138fd
ABHIN L <abhinl99@gmail.com>
2018-05-02 06:59:45 +0000

    Add new file

---
+++ server4.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env nodejs
+const http = require('http');
+const hostname = '127.0.0.4';
+const port =  process.argv[2] || 3000;
+var express = require('express'),morgan = require('morgan');
+var app = express();
+var path = require('path');
+var fs = require("fs");
+
+const server = http.createServer((req, res) => {
+fs.readFile("index.html", function(err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h1>############################ SERVING FROM SERVER 4 ##################################</h1>");
+    res.write(data);
+    res.end("<h1>############################ SERVING FROM SERVER 4 ##################################</h1>");
+  });
+
+});
+
+
+
+
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode < 400
+    }, stream: process.stderr
+}));
+
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode >= 400
+    }, stream: process.stdout
+}));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
